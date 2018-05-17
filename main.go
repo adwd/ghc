0120e4648f8c14104e53d052e0a7da7343f3fd70
adwd <ecdt.mn@gmail.com>
2018-05-17 13:16:57 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+/**
+https://docs.google.com/presentation/d/1-20sWsmtmoFrHbmsuqIzSkfQKZn7Cdj1fuNmV1Btq0o/edit#slide=id.g37c8886736_0_1079
+
+次の仕様を満たすコマンドを作って下さい
+- ディレクトリを指定する
+- 指定したディレクトリ以下のJPGファイルをPNGに変換
+- ディレクトリ以下は再帰的に処理する
+- 変換前と変換後の画像形式を指定できる
+
+以下を満たすように開発してください
+- mainパッケージと分離する
+- 自作パッケージと標準パッケージと準標準パッケージのみ使う
+  - 準標準パッケージ：golang.org/x以下のパッケージ
+- ユーザ定義型を作ってみる
+- GoDocを生成してみる
+*/
+
+func main() {
+	println("go")
+}
